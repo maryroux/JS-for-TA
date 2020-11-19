d0a5773351241aeb502bbc00b0e8fb47e1813cca
Masha Horiahina <mhoria@softserveinc.com>
2020-11-19 21:45:53 +0200

    add several tasks for lesson_3

--- lesson_3/tasks.js
+++ lesson_3/tasks.js
@@ -1,3 +1,4 @@
+//task_1
 let arr = [2, 3, 4, 5];
 let res1 = 1;
 for (let i = 0; i < arr.length; i++) {
@@ -12,3 +13,77 @@ while (i < arr.length) {
   i++;
 }
 console.log("Result_2: " + res2);
+
+//task_2
+for (let i = 0; i < 16; i++) {
+  if (i % 2 == 0) {
+    console.log(i + " is even");
+  } else {
+    console.log(i + " is odd");
+  }
+}
+
+//task_3
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randArray(k, min, max, func) {
+  let arr = [];
+  for (let i = 0; i < k; i++) {
+    arr.push(func(min, max));
+  }
+  console.log(arr);
+}
+randArray(5, 1, 500, randomNumber);
+
+//task_4
+function raiseToDegree(a, b) {
+  let numA = parseFloat(a);
+  let numB = parseFloat(b);
+  if (!Number.isInteger(numA) && !Number.isInteger(numB)) {
+    return "Enter integer numbers!";
+  }
+  return numA ** numB;
+}
+let a = prompt("Enter number a: ");
+let b = prompt("Enter number b: ");
+console.log(raiseToDegree(a, b));
+
+//task_5
+function findMin() {
+  console.log(Math.min(...arguments));
+}
+
+findMin(12, 14, 4, -4, 0.2);
+
+//task_6
+/*case_1*/
+function findUnique(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (i !== j && arr[i] === arr[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+let arrDubl = [3, 6, 7, 6, -3];
+let arrUniq = [1, 5, -9, 3, 9];
+console.log(findUnique(arrDubl));
+console.log(findUnique(arrUniq));
+
+/*case_2*/
+function arrayToSet(arr) {
+  return new Set(arr);
+}
+function isUnique(arr) {
+  return arrayToSet(arr).size === arr.length;
+}
+let arrDubl = [3, 6, 7, 6, -3];
+let arrUniq = [1, 5, -9, 3, 9];
+console.log(isUnique(arrDubl));
+console.log(isUnique(arrUniq));
